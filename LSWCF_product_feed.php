05425e1d8f7db3fb165662843b24257f1152c11f
Levanite <levanite@users.noreply.github.com>
2024-10-06 12:16:49 -0700

    Update LSWCF_product_feed.php

--- LSWCF_product_feed.php
+++ LSWCF_product_feed.php
@@ -68,16 +68,16 @@ function LSWCF_product_feed_callback() {
                     case 'ZZ':
                         $currency = ' ';
                         break;
-		    default:
-			// Default to USD if it doesn't exist in the list
-			$currency = ' USD';
-			break;
+                    default:
+                        // Default to USD if it doesn't exist in the list
+                        $currency = ' USD';
+                        break;
                 }
 
                 // Sanitize the descriptions
                 $short_description = wp_strip_all_tags($product->post_excerpt);
                 $long_description = wp_strip_all_tags($product->post_content);
-                $description = $short_description !== '' ? $short_description : $long_description;
+                $description = strlen($short_description) > 0 ? $short_description : $long_description;
 
                 $stock_status = $stock == 'instock' ? 'In stock' : 'Out of stock';
                 $stock = $variation_obj->get_stock_status();
@@ -92,8 +92,12 @@ function LSWCF_product_feed_callback() {
                 $output .= "\t\t\t" . '<g:title>' . $strip_title . '</g:title>' . PHP_EOL;
                 $output .= "\t\t\t" . '<g:description><![CDATA[' . $description . ']]></g:description>' . PHP_EOL;
                 $output .= "\t\t\t" . '<g:sku>' . $strip_sku . '</g:sku>' . PHP_EOL;
-                $output .= "\t\t\t" . '<g:image_link>' . esc_url($variation['image']['thumb_src']) . '</g:image_link>' . PHP_EOL;
-                $output .= "\t\t\t" . '<color>' . $strip_color . '</color>' . PHP_EOL;
+                $output .= "\t\t\t" . '<g:image_link>' . $variation['image']['thumb_src'] . '</g:image_link>' . PHP_EOL;
+
+                if (strlen($strip_color) > 0) {
+                    $output .= "\t\t\t" . '<color>' . $strip_color . '</color>' . PHP_EOL;
+                }
+
                 $output .= "\t\t\t" . '<g:price>' . $variation_obj->get_price() .  $currency . '</g:price>' . PHP_EOL;
                 $output .= "\t\t\t" . '<g:availability>' . $stock_status . '</g:availability>' . PHP_EOL;
                 $output .= "\t\t\t" . '<g:condition>New</g:condition>' . PHP_EOL;
@@ -101,18 +105,21 @@ function LSWCF_product_feed_callback() {
                 $output .= "\t\t\t" . '<g:item_group_id>' . $strip_sku . '</g:item_group_id>' . PHP_EOL;
 
                 // Conditional to avoid printing un-used fields.
-                if (strlen(strip_region) > 0) {
+                if (strlen($strip_region) > 0) {
                     $output .= "\t\t\t" . '<g:id>' . $strip_sku . '-' . $strip_region . '</g:id>' . PHP_EOL;
                     $output .= "\t\t\t" . '<additional_variant_attribute><label>Region</label><value>' . $strip_region . '</value></additional_variant_attribute>' . PHP_EOL;
-                    $output .= "\t\t\t" . '<g:link>' . esc_url(get_permalink( $product->ID )) . '?attribute_pa_region=' . $strip_region . '</g:link>' . PHP_EOL;
+                    $output .= "\t\t\t" . '<g:link>' . get_permalink( $product->ID ) . '?attribute_pa_region=' . $strip_region . '</g:link>' . PHP_EOL;
                     $output .= "\t\t\t" . '<g:region>' . $strip_region . '</g:region>' . PHP_EOL;
                 }
                 else {
-                    $output .= "\t\t\t" . '<g:link>' . esc_url(get_permalink( $product->ID )) . '</g:link>' . PHP_EOL;
+                    $output .= "\t\t\t" . '<g:link>' . get_permalink( $product->ID ) . '</g:link>' . PHP_EOL;
                 }
 
-                $GPID = wp_strip_all_tags($product->get_attribute( 'google-product-id' ));
-                $output .= "\t\t\t" . '<g:google_product_category>' . strlen($GPID) == 0 ? 232 : $GPID . '</g:google_product_category>' . PHP_EOL;
+                $GPID = wp_strip_all_tags($variation_obj->get_attribute( 'google-product-id' ));
+                if (strlen($GPID) > 0) {
+                    $output .= "\t\t\t" . '<g:google_product_category>' . $GPID . '</g:google_product_category>' . PHP_EOL;
+                }
+
                 $output .= "\t\t" . '</item>' . PHP_EOL;
             }
         } 
@@ -125,5 +132,5 @@ function LSWCF_product_feed_callback() {
 	exit;
 }
 
-add_filter( 'plugin_action_links_rss-for-woo-dev/LSWCF_product_feed.php', 'LSWCF_setup_view_feed_link' );
+add_filter( 'plugin_action_links_rss-for-woo/LSWCF_product_feed.php', 'LSWCF_setup_view_feed_link' );
 add_action( 'init', 'LSWCF_product_feed' );
