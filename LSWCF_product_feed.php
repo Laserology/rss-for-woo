a7ce91a74d6b62dfc4ed9493de0e9972b8e0e8e8
Levanite <levanite@users.noreply.github.com>
2024-10-10 14:55:31 -0700

    Bump version

--- LSWCF_product_feed.php
+++ LSWCF_product_feed.php
@@ -3,7 +3,7 @@
     Plugin URI: https://github.com/Laserology/woocommerce-product-feed/
     Description: Free public XML/RSS feed for your woo store.
     License: GPL v2 or later
-    Version: 1.2.1
+    Version: 1.2.2
     Author: Laserology, vladjpuscasu
     Author URI: https://laserology.net/
     Requires Plugins: woocommerce
